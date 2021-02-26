26046cd1dac240a8891b7e63338a9f88ebe4a544
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-02-25 22:31:34 -0800

    ran npm install again to create packages and node modules, created gitignore for modules, dveloped base API Call for emplyee list to generate

--- react_app/src/utils/API.js
+++ react_app/src/utils/API.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the random user API
+// Export an object containing methods we'll use 
+//for accessing the random user API 
+//to generate an employee list.
 export default {
-  
-};
+    getEmployees: function() {
+      return new Promise((resolve, reject) => {
+        axios.get("https://randomuser.me/api/?results=15").then((res) => {
+          const staff = res.data.results;
+          const results = staff.map((employee) => {
+            return {
+              firstname: employee.name.first,
+              lastname: employee.name.last,
+              fullname: employee.name.first + " " + employee.name.last,
+              email: employee.email,
+              image: employee.picture.large,
+              phone: employee.cell,
+              dob: employee.dob.date
+            };
+          });
+          console.log(results);
+          resolve(results);
+        }).catch((err) => reject(err));
+      });
+    },
+    getEmployeeList: function() {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    }
+  };
