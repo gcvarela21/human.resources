6a349498dc34aa347048d99c97a4ea66e367cc84
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-02-25 22:07:46 -0800

    re-added react app with all my un tracted -_- changes

---
+++ react_app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Directory from "./pages/Directory";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <>
+    <Directory />
+    </>
+  );
+}
+
+export default App;
---
+++ react_app/src/pages/Directory.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+// import API from "../utils/API";
+
+
+function Directory() {
+    return (
+<h1>Helloo</h1>
+    )
+};
+export default Directory;
